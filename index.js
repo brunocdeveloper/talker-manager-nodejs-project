fb8a7f127e5df3d5a1a22c5d4bf437b6e4077da0
Bruno <brunocmoraes97@gmail.com>
2021-10-06 15:47:52 -0300

    mais um teste github

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-const READING_PATH = fs.readFile('./talker.json', 'utf-8');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -23,16 +22,16 @@ app.listen(PORT, () => {
 app.get('/talker', async (_req, res) => {
   const talkers = await promiseToRead(fs.readFile('./talker.json', 'utf-8'));
   if (talkers.length < 1) return res.status(200).json([]);
-  res.status(200).send(JSON.parse(talkers));
+  res.status(200).json(JSON.parse(talkers));
 });
 
-app.get('/talker/:id', async (req, res) => {
+/* app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
-  const talkers = await promiseToRead(READING_PATH);
+  const talkers = JSON.parse(await promiseToRead(fs.readFile('./talker.json', 'utf-8')));
 
   const result = talkers.find((person) => person.id === Number(id));
 
   if (!result) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
-  return res.status(HTTP_OK_STATUS).json(result);
-});
+  return res.status(HTTP_OK_STATUS).send(result);
+}); */
