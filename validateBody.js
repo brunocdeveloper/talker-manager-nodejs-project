ecc6d1e9e44523500cc733cff6938796157be0c7
Bruno <brunocmoraes97@gmail.com>
2021-10-10 13:00:22 -0300

    tentativa de corrigir o 1

--- validateBody.js
+++ validateBody.js
@@ -3,7 +3,9 @@ const fs = require('fs').promises;
 const validateName = (req, res, next) => {
   const { name } = req.body;
 
-  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
 
   if (name.length < 3) {
     return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
@@ -15,7 +17,7 @@ const validateName = (req, res, next) => {
 const validateAge = (req, res, next) => {
   const { age } = req.body;
 
-  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (!age || age === '') return res.status(400).json({ message: 'O campo "age" é obrigatório' });
 
   if (age < 18) {
     return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
@@ -27,7 +29,7 @@ const validateAge = (req, res, next) => {
 const validateTalk = (req, res, next) => {
   const { talk } = req.body;
  
-  if (!talk || !talk.watchedAt || !talk.rate || typeof talk.rate !== 'number') {
+  if (!talk || !talk.watchedAt || !talk.rate) {
     return res.status(400).json(
       {
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
@@ -41,6 +43,7 @@ const validateTalk = (req, res, next) => {
 const validateWatchedAt = (req, res, next) => {
   const { talk: { watchedAt } } = req.body;
   const validator = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+
   if (!validator.test(watchedAt)) {
     return res.status(400).json({
       message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
@@ -53,7 +56,7 @@ const validateWatchedAt = (req, res, next) => {
 const validateRate = (req, res, next) => {
   const { talk: { rate } } = req.body;
 
-  if ((rate < 1 || rate > 5)) {
+  if (Number(rate) < 1 || Number(rate) > 5) {
     return res.status(400).json({
       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
     });
